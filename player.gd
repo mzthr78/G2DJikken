a5e29c89083722ea01230d477621413235dd8821
mzthr78 <mzthr78@gmail.com>
2026-09-08 11:36:55 +0900

    wip

--- player.gd
+++ player.gd
@@ -1,16 +1,19 @@
 extends CharacterBody2D
 
-
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
 @onready var anim_sprite = $AnimatedSprite2D
 
+@onready var debug_text = $"../TextEdit"
+
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
+	debug_text.text = str(is_on_floor())
+	
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-
+	
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
